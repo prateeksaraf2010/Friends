5e4b310ce3ef895c13ac59217e63e26802eba03c
PrateekSaraf <PrateekSaraf2010@gmail.com>
2021-02-18 17:55:28 +0530

    Final version

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
   end
   def about
+    @about_me = "My name is Prateek Saraf"
+    @answer = 2 + 2
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
   resources :friends
   get 'home/about'
   # get 'home/index'
-  root 'home#index'
+  #  root 'home#index'
+  root 'friends#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
